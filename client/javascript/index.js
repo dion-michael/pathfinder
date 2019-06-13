bbfb598e8d19bf6714c3d429e51fd4efa6b0d6ee
Dion <dionagoes@gmail.com>
2019-06-14 06:01:48 +0700

    layout done

--- client/javascript/index.js
+++ client/javascript/index.js
@@ -1,84 +1,141 @@
 const url = "http://localhost:3000";
 
-function register() {
+function register(email, password) {
+  $("#errMessage").hide()
   $.ajax({
     url: `${url}/register`,
     method: "POST",
     data: {
-      email: $("#registerEmail").val(),
-      password: $("#registerPassword").val()
+      email,
+      password
+    },
+    statusCode: {
+      201: function (user) {
+        login(email, password)
+      },
+      404: function (err) {
+        console.log(err);
+        checkLogin()
+      },
+      400: function (err) {
+        console.log(err);
+        let errObj = JSON.parse(err.responseText);
+        console.log(errObj);
+        let errMsg = Object.values(errObj)
+        $(".ui.form").removeClass("loading")
+        $("#errMessage").show()
+        $("#errMessageText").empty()
+        $("#errMessageText").append(
+          `<div class="header">
+                <ul id="errText"></ul>
+            </div>
+            <p>Please check your email / password</p>`
+        )
+        errMsg.forEach(errors => {
+          $("#errText").append(
+            `<li>${errors}</li>`)
+        })
+
+      }
     }
   })
-    .done(function(response) {
+}
+
+$('.ui.form').submit(function (event) {
+  event.preventDefault()
+  console.log($("#btnSubmit").html());
+  if ($("#btnSubmit").html() === "Login") {
+    $(".ui.form").addClass("loading")
+    let email = $(this).serializeArray()[0].value
+    let password = $(this).serializeArray()[1].value
+    login(email, password)
+  } else {
+    $(".ui.form").addClass("loading")
+    let email = $(this).serializeArray()[0].value
+    let password = $(this).serializeArray()[1].value
+    register(email, password)
+  }
+})
+
+function logout() {
+  localStorage.removeItem("token");
+  $("#map").hide()
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut()
+    .then(function () {
       showLogin();
-    })
-    .fail(function(jqXHR, textStatus) {
-      if (jqXHR.responseJSON.message) {
-        console.log(jqXHR.responseJSON.message);
-      } else {
-        console.log(jqXHR);
-      }
     });
 }
 
-function logout() {
-  localStorage.removeItem("access_token");
-  var auth2 = gapi.auth2.getAuthInstance();
-  auth2.signOut()
-  .then(function () {
-    console.log('User signed out.');
-    showLogin();
-  });
-
-}
-
-function login() {
+function login(email, password) {
+  $("#errMessage").hide()
   $.ajax({
     url: `${url}/login`,
     method: "POST",
     data: {
-      email: $("#loginEmail").val(),
-      password: $("#loginPassword").val()
+      email,
+      password
+    },
+    statusCode: {
+      200: function (user) {
+        localStorage.setItem("token", user.access_token)
+        // localStorage.setItem("email", user.email)
+        $(".ui.form").removeClass("loading")
+        checkLogin()
+      },
+      404: function (err) {
+        console.log(err);
+        $(".ui.form").removeClass("loading")
+        $("#errMessage").show()
+        $("#errMessageText").empty()
+        $("#errMessageText").append(
+          `<div class="header">
+                invalid email / password
+            </div>
+            <p>Please check your email / password</p>`
+        )
+      },
+      400: function (err) {
+        console.log(err);
+        $(".ui.form").removeClass("loading")
+        $("#errMessage").show()
+        $("#errMessageText").empty()
+        $("#errMessageText").append(
+          `<div class="header">
+                invalid email / password
+            </div>
+            <p>Please check your email / password</p>`
+        )
+      }
     }
   })
-    .done(function(response) {
-      localStorage.setItem("access_token", response.access_token);
-      showHome();
+}
+
+function onSignIn(googleUser) {
+  var id_token = googleUser.getAuthResponse().id_token;
+  $.ajax({
+      url: `${url}/google`,
+      method: 'POST',
+      data: {
+        googleToken: id_token
+      }
     })
-    .fail(function(jqXHR, textStatus) {
+    .done(function (response) {
+      localStorage.setItem('access_token', response.access_token)
+      showHome()
+    })
+    .fail(function (jqXHR, textStatus) {
       if (jqXHR.responseJSON.message) {
         console.log(jqXHR.responseJSON.message);
       } else {
         console.log(jqXHR);
       }
-    });
-}
-function onSignIn(googleUser) {
-  var id_token = googleUser.getAuthResponse().id_token;
-  console.log(id_token)
-  $.ajax({
-      url:`${url}/google`,
-      method: 'POST',
-      data: {
-          googleToken: id_token
-      }
-  })
-  .done(function(response){
-      localStorage.setItem('access_token', response.token)
-      showHome()
-  })
-  .fail(function(jqXHR, textStatus){
-    if (jqXHR.responseJSON.message) {
-      console.log(jqXHR.responseJSON.message);
-    } else {
-      console.log(jqXHR);
-    }
-  })
-  
+    })
+
 }
 
 function registerHTML() {
-    $("#registerPage").html(`
+  $("#registerPage").html(`
       <h3>Register</h3>
       <form>
           <div class="form-group">
@@ -116,9 +173,10 @@ function registerHTML() {
       });
       </script>
     `);
-  }
-  function loginHTML() {
-    $("#loginPage").html(`
+}
+
+function loginHTML() {
+  $("#loginPage").html(`
       <h3>Login</h3>
       <form>
           <div class="form-group">
@@ -156,68 +214,106 @@ function registerHTML() {
       });
       </script>
     `);
+}
 
-    gapi.signin2.render('g-signin2', {
-      'scope': 'profile email',
-      'width': 240,
-      'height': 50,
-      'longtitle': true,
-      'theme': 'dark',
-      'onsuccess': onSignIn,
-      // 'onfailure': onFailure
-    });
+
+
+$("#toggle").click(function (event) {
+  event.preventDefault()
+  toggleBtn()
+})
+
+function toggleBtn() {
+  if ($("#btnSubmit").html() === "Login") {
+    $("#errMessage").hide()
+    $("#btnSubmit").html("Register")
+    $("#toggle").html("Login")
+    $("#toggleText").html("Already registered?")
+  } else {
+    $("#errMessage").hide()
+    $("#btnSubmit").html("Login")
+    $("#toggle").html("Register")
+    $("#toggleText").html("New to us?")
   }
+}
 
-  function navbarHTML() {
-    $("#navbar").html(`
-      Group Project
-    `);
-  }
+function showLogin() {
+  $("#homePage").hide();
+  $("#registerPage").hide();
+  loginHTML();
+  $("#loginForm").show();
+  $("#navbar").show();
+}
 
-  function showLogin() {
+function showHome() {
+  $("#loginForm").hide();
+  // $("#loginPage").empty();
 
-    $("#homePage").hide();
-  
-    $("#registerPage").hide();
-    // $("#registerPage").empty();
+  $("#registerPage").hide();
+  // $("#registerPage").empty();
 
-    loginHTML();
-    $("#loginPage").show();
+  $("#homePage").show();
+  $("#navbar").show();
+  $("#map").show()
+}
 
-    navbarHTML();
-    $("#navbar").show();
-  }
-
-  function showRegister() {
-    $("#loginPage").hide();
-    // $("#loginPage").empty();
-
-    $("#homePage").hide();
-
-    registerHTML();
-    $("#registerPage").show();
-
-    navbarHTML();
-    $("#navbar").show();
-  }
-
-  function showHome() {
-    $("#loginPage").hide();
-    // $("#loginPage").empty();
-
-    $("#registerPage").hide();
-    // $("#registerPage").empty();
-
+function checkLogin() {
+  if (localStorage.getItem('token')) {
+    $("#loginForm").hide()
     $("#homePage").show();
-  
-    navbarHTML();
-    $("#navbar").show();
+  } else {
+    $("#loginForm").show()
+    $("#homePage").hide();
   }
+}
 
-  $(document).ready(function() {
-    if (localStorage.getItem("access_token")) {
-      showHome();
-    } else {
-      showLogin();
-    }
+$(document).ready(function () {
+  // if (localStorage.getItem("access_token")) {
+  //   showHome();
+  // } else {
+  //   showLogin();
+  // }
+  checkLogin()
+  renderGsignIn()
+  $('.ui.form')
+    .form({
+      fields: {
+        email: {
+          identifier: 'email',
+          rules: [{
+              type: 'empty',
+              prompt: 'Please enter your e-mail'
+            },
+            {
+              type: 'email',
+              prompt: 'Please enter a valid e-mail'
+            }
+          ]
+        },
+        password: {
+          identifier: 'password',
+          rules: [{
+              type: 'empty',
+              prompt: 'Please enter your password'
+            },
+            {
+              type: 'length[6]',
+              prompt: 'Your password must be at least 6 characters'
+            }
+          ]
+        }
+      }
+    });
+});
+
+function renderGsignIn() {
+  gapi.signin2.render('g-signin2', {
+    'scope': 'profile email',
+    'width': 215,
+    'height': 50,
+    'longtitle': true,
+    'theme': 'light',
+    'onsuccess': onSignIn,
+    // 'onfailure': onFailure
   });
+}
