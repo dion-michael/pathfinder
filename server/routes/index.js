12af2b4fdd0d91c5e0e692709639eb145a8b24f2
melinadewi <hds.melina@gmail.com>
2019-06-14 01:47:17 +0700

    All Trafi Links

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,9 @@
 const routes = require("express").Router()
 const user = require("./user.js")
+const trafi = require("./trafi.js")
 
 routes.use('/', user)
-
+routes.use('/t', trafi)
 
 
 module.exports = routes
